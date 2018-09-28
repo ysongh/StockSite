4188015510daedf51ed114791338a753a1f2b38b
Song <yousonghou@yahoo.com>
2018-09-28 15:39:53 +0000

    add user login route with jsonwebtoken

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const router = express.Router();
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const keys = require('../../config/keys');
 
 const User = require('../../models/User');
 
@@ -28,6 +30,38 @@ router.post('/register', (req, res) => {
                     });
                 });
             }
+        });
+});
+
+router.post('/login', (req, res) => {
+    const email = req.body.email;
+    const password = req.body.password;
+    
+    User.findOne({email})
+        .then(user => {
+            if(!user){
+                return res.status(404).json({email: 'User email not found'});
+            }
+            bcrypt.compare(password, user.password)
+                .then(isMatch => {
+                    if(isMatch){
+                        const payload = {id: user.id, name: user.name};
+                        
+                        jwt.sign(
+                            payload,
+                            keys.secretOrKey,
+                            {expiresIn: 3600},
+                            (err, token) => {
+                                res.json({
+                                    success: true,
+                                    token: 'Bearer ' + token
+                                });
+                        });
+                    }
+                    else{
+                        return res.status(400).json({password: 'Password Incorrect'});
+                    }
+                });
         });
 });
 
