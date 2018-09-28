1bd9dad496a654053cc3880d4184e2ffd57954d9
Song <yousonghou@yahoo.com>
2018-09-28 20:52:35 +0000

    set up react-dom-router

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import './App.css';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 
 import Home from './component/home/Home';
 import Register from './component/auth/Register';
@@ -8,13 +8,15 @@ import Portfolio from './component/Portfolio';
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <div className="container">
-          <Home />
-          <Portfolio />
-          <Register/>
+      <Router>
+        <div className="App">
+          <div className="container">
+            <Route exact path="/" component={Home} />
+            <Route exact path="/register" component={Register} />
+            <Route exact path="/portfolio" component={Portfolio} />
+          </div>
         </div>
-      </div>
+      </Router>
     );
   }
 }
--- client/src/component/home/Home.js
+++ client/src/component/home/Home.js
@@ -1,10 +1,14 @@
 import React, {Component} from 'react';
+import { Link } from 'react-router-dom';
 
 class Home extends Component{
   render(){
       return(
-          <div>
-              <h1 className="text-center">Stock Site</h1>
+          <div className="text-center">
+              <h1>Stock Site</h1>
+              <Link to="/register" className="btn btn-light">
+                  Sign Up
+              </Link>
           </div>
       );
   }
