3d03acfb96307a8457f42ddc06d341e1fbeb7dc9
Song <yousonghou@yahoo.com>
2018-09-28 04:47:36 +0000

    set up connection to MLab

--- server.js
+++ server.js
@@ -1,7 +1,14 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 
 const app = express();
+
+const db = require('./config/keys').mongoURI;
+ mongoose
+    .connect(db, { useNewUrlParser: true })
+    .then(() => console.log('MongoDB Connected'))
+    .catch(err => console.log(err));
 
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
