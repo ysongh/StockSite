7e911b611d0558b3983d1e5dbe977a828ded10aa
Song <yousonghou@yahoo.com>
2018-09-28 20:28:37 +0000

    add passport to server.js

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
+const passport = require('passport');
 
 const users = require('./routes/api/users');
 
@@ -14,6 +15,9 @@ const db = require('./config/keys').mongoURI;
 
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
+
+app.use(passport.initialize());
+require('./config/passport')(passport);
 
 app.use('/api/users', users);
 
