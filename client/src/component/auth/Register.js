557eee4b87330e3fff60b69d820057163dc6d163
Song <yousonghou@yahoo.com>
2018-09-29 18:32:11 +0000

    add history to register

--- client/src/component/auth/Register.js
+++ client/src/component/auth/Register.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import classnames from 'classnames';
 import { connect } from 'react-redux';
+import { withRouter  } from 'react-router-dom';
 
 import { registerUser } from '../../actions/authActions';
 
@@ -37,7 +38,7 @@ class Register extends Component{
             password2: this.state.password2
         };
         
-        this.props.registerUser(newUser);
+        this.props.registerUser(newUser, this.props.history);
     }
     
     render(){
@@ -109,4 +110,4 @@ const mapStateToProps = state => ({
   errors: state.errors
 });
 
-export default connect(mapStateToProps, {registerUser})(Register); 
+export default connect(mapStateToProps, {registerUser})(withRouter(Register));  
