1d50e6540e4b9b0dc4f67f39e5a43d993e8695a9
Song <yousonghou@yahoo.com>
2018-09-29 18:58:18 +0000

    add redux login action and set current user

--- client/src/reducers/authReducer.js
+++ client/src/reducers/authReducer.js
@@ -1,3 +1,5 @@
+import isEmpty from '../validation/is-empty';
+
 const initialState = {
     isAuthenticated: false,
     user: {}
@@ -5,6 +7,12 @@ const initialState = {
 
 export default function(state = initialState, action){
     switch(action.type){
+        case 'SET_CURRENT_USER':
+            return{
+                ...state,
+                isAuthenticated: !isEmpty(action.payload),
+                user: action.payload
+            };
         default:
             return state;
     }
