59267e43ba8d52a301a4790897a0def477698c86
Song <yousonghou@yahoo.com>
2018-10-01 16:13:06 +0000

    add conditional buttons and image on home component

--- client/src/component/home/Home.js
+++ client/src/component/home/Home.js
@@ -1,20 +1,44 @@
 import React, {Component} from 'react';
 import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
+
+import stockImage from '../../img/stocks.png';
 
 class Home extends Component{
   render(){
+      const {isAuthenticated} = this.props.auth;
+      
+      const authLinks = (
+        <div>
+          <Link to="/portfolio" className="btn btn-light">
+              Portfolio
+          </Link>
+        </div>
+      );
+      
+      const guestLinks = (
+        <div>
+          <Link to="/login" className="btn btn-light">
+              Login
+          </Link>
+          <Link to="/register" className="btn btn-light">
+              Sign Up
+          </Link>
+        </div>
+      );
+      
       return(
           <div className="text-center">
               <h1>Stock Site</h1>
-              <Link to="/login" className="btn btn-light">
-                  Login
-              </Link>
-              <Link to="/register" className="btn btn-light">
-                  Sign Up
-              </Link>
+              <img className="img-fluid" style={{width: '500px', height: '350px'}} src={stockImage} alt="Stocks" />
+              {isAuthenticated ? authLinks : guestLinks}
           </div>
       );
   }
 }
 
-export default Home;
+const mapStateToProps = (state) => ({
+  auth: state.auth
+});
+
+export default connect(mapStateToProps)(Home);
